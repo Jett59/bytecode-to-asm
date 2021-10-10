33588779174fe6475caf24495079da0e4ebe92c3
Jett Thompson <jett.thompson.59@gmail.com>
2021-10-10 16:42:17 +1100

    created the assembly writer which just prints some initial text

--- src/main/java/app/cleancode/bytecode_to_asm/App.java
+++ src/main/java/app/cleancode/bytecode_to_asm/App.java
@@ -1,12 +1,16 @@
 package app.cleancode.bytecode_to_asm;
 
 import org.objectweb.asm.ClassReader;
+import app.cleancode.bytecode_to_asm.asm.AssemblyWriter;
 
 public class App {
     public static void main(String[] args) {
         try {
             ClassReader reader = new ClassReader(Test.class.getName());
-            reader.accept(new ClassInspector(), 0);
+            ClassInspector inspector = new ClassInspector();
+            reader.accept(inspector, 0);
+            AssemblyWriter assemblyWriter = new AssemblyWriter();
+            assemblyWriter.writeAssembly(inspector.info, System.err, "Test.S");
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/app/cleancode/bytecode_to_asm/asm/AssemblyWriter.java
+++ src/main/java/app/cleancode/bytecode_to_asm/asm/AssemblyWriter.java
@@ -1,5 +1,20 @@
 package app.cleancode.bytecode_to_asm.asm;
 
-public class AssemblyWriter {
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import app.cleancode.bytecode_to_asm.ClassInfo;
 
+public class AssemblyWriter {
+    public void writeAssembly(ClassInfo classInfo, OutputStream output, String fileName) {
+        try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(output);
+                BufferedWriter writer = new BufferedWriter(outputStreamWriter)) {
+            writer.append(String.format("/*Compiled from: %s*/\n\n", classInfo.name()));
+            writer.append(String.format(".file \"%s\"\n\n", fileName));
+            writer.append(".code64\n\n");
+            writer.append(".text\n\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
