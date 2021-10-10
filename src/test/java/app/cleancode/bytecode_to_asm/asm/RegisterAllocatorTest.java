9152d3126827679449b3b2815e01dd556faa9114
Jett Thompson <jett.thompson.59@gmail.com>
2021-10-10 16:27:34 +1100

    made the test test that the freeing of registers works

--- src/test/java/app/cleancode/bytecode_to_asm/asm/RegisterAllocatorTest.java
+++ src/test/java/app/cleancode/bytecode_to_asm/asm/RegisterAllocatorTest.java
@@ -2,6 +2,7 @@ package app.cleancode.bytecode_to_asm.asm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class RegisterAllocatorTest {
@@ -12,5 +13,18 @@ public class RegisterAllocatorTest {
             assertNotEquals(allocator.allocate(), null);
         }
         assertEquals(null, allocator.allocate());
+    }
+
+    @Test
+    public void allocate_withFree() {
+        RegisterAllocator allocator = new RegisterAllocator();
+        for (int i = 0; i < Register.values().length; i++) {
+            assertNotEquals(allocator.allocate(), null);
+        }
+        assertEquals(null, allocator.allocate());
+        allocator.free(Register.RDI);
+        assertEquals(Register.RDI, allocator.allocate());
+        allocator.free(Register.RDI);
+        assertThrows(IllegalArgumentException.class, () -> allocator.free(Register.RDI));
     }
 }
